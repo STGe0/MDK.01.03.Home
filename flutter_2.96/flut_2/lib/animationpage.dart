ecb0aa333aed5c68fa2108cd644366fc1d732215
STGeo <91418802+STGe0@users.noreply.github.com>
2022-05-27 17:26:59 +0300

    2.96_2+

---
+++ flutter_2.96/flut_2/lib/animationpage.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as Match;
+
+class AnimationPage extends StatelessWidget{
+  const AnimationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text('Анимация')),
+      body: Center(
+        child: AnimationWidget(),
+      ),
+    );
+  }
+}
+
+class AnimationWidget extends StatefulWidget{
+   const AnimationWidget({
+    Key? key,
+  }) : super (key: key);
+
+  @override
+  State<AnimationWidget> createState() => _AnimationWidgetState();
+}
+
+class _AnimationWidgetState extends State<AnimationWidget>
+with SingleTickerProviderStateMixin{
+  late AnimationController controller;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+    duration: Duration(seconds: 3),);
+    _animation = Tween<Offset>(begin: Offset.zero, end: Offset(2.1, 0.0),).animate(CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn));
+
+    controller.addListener(() {
+      if(controller.status == AnimationStatus.completed){
+
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    controller.forward();
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child){
+        return Align(
+          alignment: Alignment.centerLeft,
+          child: SlideTransition(
+            position: _animation,
+            child: Container(
+              width: 300,
+              height: 200,
+              decoration: BoxDecoration(
+                image: new DecorationImage(
+                    image: new AssetImage("images/1.png")),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
